64a821c676b50728503f0cca757df64337602e8e
Joao Fonseca <jf-fonsec@hotmail.com>
2024-10-30 22:19:12 -0300

    tela principal

---
+++ preload.js
@@ -0,0 +1,12 @@
+/**
+ * Segurança e Desempenho
+ */
+
+const { contextBridge, ipcRenderer} = require('electron')
+
+// Processos de comunicação entre renderer e main
+contextBridge.exposeInMainWorld('api', {
+    // A linha abaixo cria uma função que envia uma mensagem ao processo principal
+    fecharJanela: () => ipcRenderer.send('close-about'), 
+    janelaClientes: () => ipcRenderer.send('open-client')
+})
---
+++ src/views/renderer.js
@@ -0,0 +1,15 @@
+/**
+ * Processos de Renderização
+ */
+
+//Botões
+
+// Botão OK (janela sobre)
+function fechar() {
+    api.fecharJanela()
+}
+
+function clientes() {
+    //alert ("teste do botão clientes")
+    api.janelaClientes()
+}
