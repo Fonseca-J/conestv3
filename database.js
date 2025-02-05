189370c1aab498ae3744e33dc989bc4959ba61d1
Fonseca-J <148169935+Fonseca-J@users.noreply.github.com>
2025-02-05 19:52:30 -0300

    Add files via upload

--- database.js
+++ database.js
@@ -5,46 +5,47 @@
 
 const mongoose = require('mongoose')
 
-// definir a URL e autenticação do banco de dados 
+// Definir a URl e autenticação do banco de dados (acrescentar ao final da URL um nome para o banco de dados)
 const url = 'mongodb+srv://admin:Luzo0715@conest.0ul5b.mongodb.net/dbfonsconest' // ao final da URL acrescentar o nome do banco de dados
 
-// Status de conexão (icone de conexão)
+
+// status de conexão ("ícone de conexão")
 let isConnected = false
 
-// Só estabelecer uma nova conexão se não estiver conectado
+// só estabelecer uma conexão se não estiver conectado
 const dbConnect = async () => {
     if (isConnected === false) {
         await conectar()
     }
 }
 
-// Conectar
+// conectar
 const conectar = async () => {
     if (isConnected === false) {
         try {
-            // alinha abaixo abre a conexão com o MongoDB
+            // A linha abaixo abre a conexão com o MongoDB
             await mongoose.connect(url)
             isConnected = true // sinalizar que o banco está conectado
-            console.log('MongoDB conectado')
-        } catch (erro) {
-            console.log(`Problema detectado: ${error}`)
+            console.log("MongoDB conectado")
+        } catch (error) {
+            console.log(`Problema Detectado: ${error}`)
         }
     }
 }
 
-// Desconectar
+// desconectar
 const desconectar = async () => {
     if (isConnected === true) {
         try {
-            // alinha abaixo encerra a conexão com o MongoDB
+            // A linha encerra a conexão com o MongoDB
             await mongoose.disconnect(url)
             isConnected = false // sinalizar que o banco não está conectado
-            console.log('MongoDB desconectado')
-        } catch (erro) {
-            console.log(`Problema detectado: ${error}`)
+            console.log("MongoDB desconectado")
+        } catch (error) {
+            console.log(`Problema Detectado: ${error}`)
         }
     }
 }
 
-// Exportar para o "main" as funções desejadas
-module.exports = {dbConnect, desconectar}
+// exportar para o main as funções desejadas
+module.exports = { dbConnect, desconectar }
